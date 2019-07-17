fa0508779e75d1f59c04c12487bc124a66167e98
Evette Telyas <etelyas@gmail.com>
2019-07-17 14:22:06 -0600

    initial commit

---
+++ lib/museum.rb
@@ -0,0 +1,13 @@
+class Museum
+  attr_reader :name, :exhibits
+
+  def initialize(name)
+    @name = name
+    @exhibits = []
+  end
+
+  def recommend_exhibits(patron)
+    
+  end
+
+end
---
+++ test/museum_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+require 'pry'
+
+class MuseumTest < Minitest::Test
+
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    @dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    @imax = Exhibit.new("IMAX", 15)
+    @bob = Patron.new("Bob", 20)
+    @sally = Patron.new("Sally", 20)
+  end
+
+  def test_museum_exist
+    assert_instance_of Museum, @dmns
+  end
+
+  def test_attributes
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+    assert_equal [], @dmns.exhibits
+  end
+
+
+
+
+
+end
