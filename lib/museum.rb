1301137fba69d02d219550f9b0e9dcae739f376d
Evette Telyas <etelyas@gmail.com>
2019-07-17 14:33:59 -0600

    admit patrons

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,13 +1,28 @@
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits.push(exhibit)
   end
 
   def recommend_exhibits(patron)
-    
+    recommendations = []
+    @exhibits.each do |exhibit|
+      if patron.interests.include?(exhibit.name)
+        recommendations << exhibit
+      end
+    end
+    recommendations
+  end
+
+  def admit(patron)
+    @patrons.push(patron)
   end
 
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -23,6 +23,33 @@ class MuseumTest < Minitest::Test
   def test_attributes
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
+    assert_equal [], @dmns.patrons
+  end
+
+  def test_add_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+
+  def test_recommend_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+    @sally.add_interest("IMAX")
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@bob)
+    assert_equal  [@imax], @dmns.recommend_exhibits(@sally)
+  end
+
+  def test_admit_patrons
+    @dmns.admit(@bob)
+    @dmns.admit(@sally)
+
+    assert_equal [@bob, @sally], @dmns.patrons
   end
 
 
